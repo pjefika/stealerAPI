e19a3d4efd13e9594a592717aa563c9189ec84be
Henrique Merlin <henmerlin@gmail.com>
2017-09-20 16:09:23 -0300

    factoryIT Service fix

--- stealerAPI/src/java/dao/FactoryITService.java
+++ stealerAPI/src/java/dao/FactoryITService.java
@@ -9,11 +9,6 @@ package dao;
 import br.com.gvt.oss.inventory.service.impl.*;
 import com.gvt.www.ws.eai.oss.ossturbonet.OSSTurbonet;
 import com.gvt.www.ws.eai.oss.ossturbonet.OSSTurbonetProxy;
-import com.sun.xml.internal.ws.client.BindingProviderProperties;
-import java.net.MalformedURLException;
-import java.net.URL;
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
 
 /**
  *
