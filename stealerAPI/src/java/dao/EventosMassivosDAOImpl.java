ae16c98dc291cbc3a99903f266d45fdb0d4c653e
Agibert <matheusagibert@gmail.com>
2017-12-20 17:15:05 -0200

    EfikaThread para eventos massivos done (t6 na serviceImpl)

--- stealerAPI/src/java/dao/EventosMassivosDAOImpl.java
+++ stealerAPI/src/java/dao/EventosMassivosDAOImpl.java
@@ -7,13 +7,51 @@ package dao;
 
 import br.net.gvt.efika.customer.EfikaCustomer;
 import br.net.gvt.efika.customer.EventoMassivo;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import model.entity.EventosMassivos;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import util.GsonUtil;
 
 public class EventosMassivosDAOImpl extends HttpDAO implements EventosMassivosDAO {
 
     @Override
     public List<EventoMassivo> consultar(EfikaCustomer cust) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        HttpClient httpcliente = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost("http://10.40.195.81:8080/plrestAPI/eventosMassivos/afetaCliente");
+
+        // Request parameters and other properties.
+        StringEntity param = new StringEntity("{\"cust\": " + GsonUtil.serialize(cust) + " , \"executor\": \"stealerAPI\"}");
+        httppost.addHeader("content-type", "application/json");
+        httppost.setEntity(param);
+
+        //Execute and get the response.
+        HttpResponse response = httpcliente.execute(httppost);
+        HttpEntity entity = response.getEntity();
+
+        InputStream instream = entity.getContent();
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(entity.getContent()));
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+
+        }
+
+        instream.close();
+        Gson g = new Gson();
+        EventosMassivos evM = g.fromJson(result.toString(), EventosMassivos.class);
+
+        return evM.getEventos();
     }
 
 }
