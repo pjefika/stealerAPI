c754a46dea63c66079bae81265b106d8c2eac8e3
Agibert <matheusagibert@gmail.com>
2018-01-26 16:02:37 -0200

    setting transient address for reference in TratativaInventarioServicos

--- stealerAPI/test/model/service/EfikaCustomerServiceImplIT.java
+++ stealerAPI/test/model/service/EfikaCustomerServiceImplIT.java
@@ -19,30 +19,31 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import util.GsonUtil;
+import util.JacksonMapper;
 
 /**
  *
  * @author G0042204
  */
 public class EfikaCustomerServiceImplIT {
-
+    
     private final String param = "CTA-301CA4208-013";
-
+    
     public EfikaCustomerServiceImplIT() {
     }
-
+    
     @BeforeClass
     public static void setUpClass() {
     }
-
+    
     @AfterClass
     public static void tearDownClass() {
     }
-
+    
     @Before
     public void setUp() {
     }
-
+    
     @After
     public void tearDown() {
     }
@@ -59,11 +60,11 @@ public class EfikaCustomerServiceImplIT {
             assertTrue("nulo", result != null);
             assertTrue("rede externa", result.getRedeExterna() != null);
             assertTrue("rede", result.getRede() != null);
-            System.out.println(GsonUtil.serialize(result));
+            System.out.println(new JacksonMapper(EfikaCustomer.class).serialize(result));
         } catch (Exception e) {
             fail(e.getMessage());
         }
-
+        
     }
 
     /**
@@ -125,5 +126,5 @@ public class EfikaCustomerServiceImplIT {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-
+    
 }
