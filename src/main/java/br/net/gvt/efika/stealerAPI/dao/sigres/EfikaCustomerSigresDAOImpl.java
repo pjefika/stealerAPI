82ca491e15d4655526b55a0f8a5f7f96d48dabf1
Henrique Merlin <henmerlin@gmail.com>
2018-02-17 11:48:43 -0200

    add terminal consulta

--- src/main/java/br/net/gvt/efika/stealerAPI/dao/sigres/EfikaCustomerSigresDAOImpl.java
+++ src/main/java/br/net/gvt/efika/stealerAPI/dao/sigres/EfikaCustomerSigresDAOImpl.java
@@ -11,6 +11,7 @@ import br.net.gvt.efika.customer.model.customer.enums.OrigemPlanta;
 import br.net.gvt.efika.customer.model.customer.enums.OrigemRede;
 import br.net.gvt.efika.customer.model.customer.enums.TipoRede;
 import br.net.gvt.efika.stealerAPI.dao.InventarioRedeDAO;
+import br.net.gvt.efika.stealerAPI.dao.exception.InstanciaInvalidaException;
 import br.net.gvt.efika.util.regex.EfikaRegex;
 import org.jsoup.Connection;
 import org.jsoup.Connection.Response;
@@ -22,47 +23,76 @@ public class EfikaCustomerSigresDAOImpl implements EfikaCustomerSigresDAO, Inven
 
     private Response login;
 
-    @Override
-    public EfikaCustomer consultar(EfikaCustomer cust) throws Exception {
+    private Document doc;
 
-        Document doc;
+    protected String consultarPorIdFibra(String idFibra) throws Exception {
         try {
             doc = Jsoup.connect("http://192.168.236.92/portal/consultacliente.do")
                     .timeout(10000)
                     .header("Content-Type", "application/x-www-form-urlencoded")
                     .data("pagina_destino", "consulta_terminal")
                     .data("tipoConsulta", "porLp")
-                    .data("Lp", cust.getInstancia())
+                    .data("Lp", idFibra)
                     .cookies(getLogin().cookies())
                     .post();
-
-            Elements select = doc.select("table td.bgform");
-            String result = select.get(18).text();
-            System.out.println("result: " + result);
-            try {
-                InventarioRede rede = new InventarioRede();
-                rede.setTipo(TipoRede.GPON);
-                rede.setOrigem(OrigemRede.ONLINE);
-                rede.setPlanta(OrigemPlanta.VIVO1);
-                rede.setNrc(EfikaRegex.capture(result, "(?:NRC.)(\\w{5,10})", 1));
-                rede.setIpDslam(EfikaRegex.capture(result, "(?:Equipamento.)(BR_\\w{10,20})", 1));
-                rede.setIdOnt(EfikaRegex.capture(result, "(?:Id ONT\\s{0,3})(.{10})", 1));
-                rede.setTerminal(EfikaRegex.capture(result, "(?:Terminal.)\\s{0,3}(\\d{10,20})", 1));
-                rede.setSlot(new Integer(EfikaRegex.capture(result, "(?:Cartão.{0,5}Cartão\\s{0,3})(\\d{0,2})", 1)));
-                rede.setPorta(new Integer(EfikaRegex.capture(result, "(?:Porta.{0,5}Porta\\s{0,3})(\\d{0,2})", 1)));
-                rede.setRin(new Integer(EfikaRegex.capture(result, "(?:VLan de Rede\\s{0,3})(\\d{0,4})", 1)));
-                rede.setCvlan(new Integer(EfikaRegex.capture(result, "(?:VLan do Usuário\\s{0,3})(\\d{0,3})", 1)));
-                rede.setBhs(EfikaRegex.capture(result, "(?:BHS/HGU Instalado\\s{0,3})(\\w{0,3})", 1).equalsIgnoreCase("SIM"));
-                rede.setLogica(new Integer(EfikaRegex.capture(result, "(?:Id do Cliente\\s{0,3})(\\w{0,3})", 1)));
-                rede.setSequencial(rede.getLogica());
-                cust.setRede(rede);
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new Exception("Falha ao tratar informações do SIGRES!");
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            return doc.select("table td.bgform").get(18).text();
+        } catch (Exception e) {
+            e.printStackTrace();
             throw new Exception("Falha ao consultar SIGRES!");
+        }
+    }
+
+    protected String consultarPorTerminal(String terminal) throws Exception {
+        try {
+            doc = Jsoup.connect("http://192.168.236.92/portal/consultacliente.do")
+                    .timeout(10000)
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .data("pagina_destino", "consulta_terminal")
+                    .data("tipoConsulta", "porTerminal")
+                    .data("Lp", terminal)
+                    .cookies(getLogin().cookies())
+                    .post();
+            return doc.select("table td.bgform").get(18).text();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("Falha ao consultar SIGRES!");
+        }
+    }
+
+    @Override
+    public EfikaCustomer consultar(EfikaCustomer cust) throws Exception {
+        String result = "";
+        if (cust.getInstancia().length() < 15) {
+            result = this.consultarPorTerminal(cust.getInstancia());
+        } else {
+            result = this.consultarPorIdFibra(cust.getInstancia());
+        }
+
+        System.out.println("result: " + result);
+        if (result.contains("Terminal não encontrado")) {
+            throw new InstanciaInvalidaException();
+        }
+
+        try {
+            InventarioRede rede = new InventarioRede();
+            rede.setTipo(TipoRede.GPON);
+            rede.setOrigem(OrigemRede.ONLINE);
+            rede.setPlanta(OrigemPlanta.VIVO1);
+            rede.setNrc(EfikaRegex.capture(result, "(?:NRC.)(\\w{5,10})", 1));
+            rede.setIpDslam(EfikaRegex.capture(result, "(?:Equipamento.)(BR_\\w{10,20})", 1));
+            rede.setIdOnt(EfikaRegex.capture(result, "(?:Id ONT\\s{0,3})(.{10})", 1));
+            rede.setTerminal(EfikaRegex.capture(result, "(?:Terminal.)\\s{0,3}(\\d{10,20})", 1));
+            rede.setSlot(new Integer(EfikaRegex.capture(result, "(?:Cartão.{0,5}Cartão\\s{0,3})(\\d{0,2})", 1)));
+            rede.setPorta(new Integer(EfikaRegex.capture(result, "(?:Porta.{0,5}Porta\\s{0,3})(\\d{0,2})", 1)));
+            rede.setRin(new Integer(EfikaRegex.capture(result, "(?:VLan de Rede\\s{0,3})(\\d{0,4})", 1)));
+            rede.setCvlan(new Integer(EfikaRegex.capture(result, "(?:VLan do Usuário\\s{0,3})(\\d{0,3})", 1)));
+            rede.setBhs(EfikaRegex.capture(result, "(?:BHS/HGU Instalado\\s{0,3})(\\w{0,3})", 1).equalsIgnoreCase("SIM"));
+            rede.setLogica(new Integer(EfikaRegex.capture(result, "(?:Id do Cliente\\s{0,3})(\\w{0,3})", 1)));
+            rede.setSequencial(rede.getLogica());
+            cust.setRede(rede);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("Falha ao tratar informações do SIGRES!");
         }
 
         return cust;
