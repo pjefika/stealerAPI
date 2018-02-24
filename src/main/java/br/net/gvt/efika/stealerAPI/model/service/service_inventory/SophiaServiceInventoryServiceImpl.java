922b031b447902b87a826431a6b4e526405da261
Henrique Merlin <henmerlin@gmail.com>
2018-02-24 12:05:14 -0300

    fix deps.

--- src/main/java/br/net/gvt/efika/stealerAPI/model/service/service_inventory/SophiaServiceInventoryServiceImpl.java
+++ src/main/java/br/net/gvt/efika/stealerAPI/model/service/service_inventory/SophiaServiceInventoryServiceImpl.java
@@ -7,23 +7,25 @@ package br.net.gvt.efika.stealerAPI.model.service.service_inventory;
 
 import br.net.gvt.efika.efika_customer.model.customer.EfikaCustomer;
 import br.net.gvt.efika.efika_customer.model.customer.enums.OrigemInventarioServico;
+import br.net.gvt.efika.stealerAPI.dao.FactoryDAO;
 import com.gvt.ws.eai.oss.inventory.api.InventoryAccountResponse;
 import com.gvt.ws.eai.oss.inventory.api.InventoryDesignatorsResponse;
 import br.net.gvt.efika.stealerAPI.dao.exception.InstanciaInvalidaException;
 import br.net.gvt.efika.stealerAPI.model.service.tratativa.TratativaAssociatedDesignators;
 import br.net.gvt.efika.stealerAPI.model.service.tratativa.TratativaInventarioServicos;
 import br.net.gvt.efika.stealerAPI.dao.exception.ImpossivelIdentificarDesignadoresException;
-import br.net.gvt.efika.stealerAPI.model.service.OSSV2GenericService;
+import br.net.gvt.efika.stealerAPI.dao.oss.OSSGenericDAO;
 
-public class SophiaServiceInventoryServiceImpl extends OSSV2GenericService implements ServiceInventoryService {
+public class SophiaServiceInventoryServiceImpl implements ServiceInventoryService {
 
     private EfikaCustomer ec;
 
     @Override
     public synchronized EfikaCustomer consultar(String instancia) throws Exception {
         ec = new EfikaCustomer();
-        InventoryAccountResponse accountItems = getDAO().getAccountItems(instancia);
-        InventoryDesignatorsResponse associatedDesignators = getDAO().getAssociatedDesignators(instancia);
+        OSSGenericDAO oss = FactoryDAO.createOSS();
+        InventoryAccountResponse accountItems = oss.getAccountItems(instancia);
+        InventoryDesignatorsResponse associatedDesignators = oss.getAssociatedDesignators(instancia);
         try {
             new TratativaAssociatedDesignators(associatedDesignators, ec, accountItems).run();
             new TratativaInventarioServicos(accountItems, ec).run();
