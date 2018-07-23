5e0a934ee78f21d846dc9cde5b7ded4dae3f9379
Agibert <matheusagibert@gmail.com>
2018-07-23 20:32:47 -0300

    requestBombandin

---
+++ src/main/java/br/net/gvt/efika/stealerAPI/dao/assia/AssiaDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.net.gvt.efika.stealerAPI.dao.assia;
+
+import com.assia.dslo.realtime.api.xsd.RealtimeRequest;
+
+/**
+ *
+ * @author G0041775
+ */
+public abstract class AssiaDAO {
+
+    public abstract void dummy() throws Exception;
+
+}
