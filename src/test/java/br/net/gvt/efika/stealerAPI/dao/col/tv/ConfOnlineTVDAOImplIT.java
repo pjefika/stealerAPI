b1d00a2f01f530ac2aacdb57bf7a3d0cbc0e4b8c
Agibert <matheusagibert@gmail.com>
2018-10-29 17:24:42 -0300

    starting branch

--- src/test/java/br/net/gvt/efika/stealerAPI/dao/col/tv/ConfOnlineTVDAOImplIT.java
+++ src/test/java/br/net/gvt/efika/stealerAPI/dao/col/tv/ConfOnlineTVDAOImplIT.java
@@ -5,14 +5,10 @@
  */
 package br.net.gvt.efika.stealerAPI.dao.col.tv;
 
-import br.net.gvt.efika.stealer.model.tv.DecoderTV;
-import br.net.gvt.efika.util.json.JacksonMapper;
-import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
 
 /**
  *
@@ -38,27 +34,26 @@ public class ConfOnlineTVDAOImplIT {
     @After
     public void tearDown() {
     }
-    ConfOnlineTVDAOImpl instance = new ConfOnlineTVDAOImpl();
-
-    /**
-     * Test of getStb method, of class ConfOnlineTVDAOImpl.
-     */
-    @Test
-    public void testGetStb() throws Exception {
-        System.out.println("getStb");
-        List<DecoderTV> result = instance.getStb("TV-CTA-813PE2SDIU-050");
-        System.out.println(new JacksonMapper(List.class).serialize(result));
-    }
-
-    /**
-     * Test of getStbDiagnostics method, of class ConfOnlineTVDAOImpl.
-     */
-    @Test
-    public void testGetStbDiagnostics() throws Exception {
-        System.out.println("getStbDiagnostics");
-        ConfOnlineTVDAOImpl instance = new ConfOnlineTVDAOImpl();
-        List<DecoderTV> result = instance.getStbDiagnostics("AMR-814Y1Z4G36-013", "TV-AMR-814Y1Z4G38-050");
-        System.out.println(new JacksonMapper(List.class).serialize(result));
-    }
-
+//    ConfOnlineTVDAOImpl instance = new ConfOnlineTVDAOImpl();
+//
+//    /**
+//     * Test of getStb method, of class ConfOnlineTVDAOImpl.
+//     */
+//    @Test
+//    public void testGetStb() throws Exception {
+////        System.out.println("getStb");
+////        List<DecoderTV> result = instance.getStb("TV-CTA-813PE2SDIU-050");
+////        System.out.println(new JacksonMapper(List.class).serialize(result));
+////    }
+//
+//    /**
+//     * Test of getStbDiagnostics method, of class ConfOnlineTVDAOImpl.
+//     */
+//    @Test
+//    public void testGetStbDiagnostics() throws Exception {
+////        System.out.println("getStbDiagnostics");
+////        ConfOnlineTVDAOImpl instance = new ConfOnlineTVDAOImpl();
+////        List<DecoderTV> result = instance.getStbDiagnostics("AMR-814Y1Z4G36-013", "TV-AMR-814Y1Z4G38-050");
+////        System.out.println(new JacksonMapper(List.class).serialize(result));
+//    }
 }
